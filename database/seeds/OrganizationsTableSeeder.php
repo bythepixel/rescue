618dee401501990abe6fc1b4b0de1b42f2495574
Turner Walters <turner@bythepixel.com>
2018-09-27 14:59:31 -0600

    Some seeders

---
+++ database/seeds/OrganizationsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrganizationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('organizations')->insert([
+            'name' => 'By the Pixel'
+        ]);
+        DB::table('organizations')->insert([
+            'name' => 'Rescue R Us'
+        ]);
+        DB::table('organizations')->insert([
+            'name' => 'DEAR GOD HELP THEM'
+        ]);
+        DB::table('organizations')->insert([
+            'name' => 'Don\'t let the dogs die'
+        ]);
+        DB::table('organizations')->insert([
+            'name' => 'Dead Ones'
+        ]);
+    }
+}
