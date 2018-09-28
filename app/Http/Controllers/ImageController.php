fbccb5a2b79cb15031aa22eedcbbdda4baf0f9d2
Turner Walters <turner@bythepixel.com>
2018-09-28 14:10:54 -0600

    Update ImageController

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Image;
+use App\Services\ImageService;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -13,12 +14,17 @@ class ImageController extends Controller
      *
      * @param Request $request
      * @return Image
+     * @throws \League\Flysystem\FileExistsException
      */
     public function store(Request $request)
     {
         $image = new Image();
 
-        $image->path = $request->input('name');
+        $imageFile = $request->file('image');
+        $imageService = new ImageService();
+        $imageService->upload('/', $imageFile);
+
+        $image->path = $imageFile->path();
         $image->pet_id = $request->input('pet_id');
 
         $image->save();
@@ -32,10 +38,15 @@ class ImageController extends Controller
      * @param Request $request
      * @param int $id
      * @return Image
+     * @throws \League\Flysystem\FileNotFoundException
      */
     public function update(int $id, Request $request)
     {
         $image = Image::find($id);
+
+        $imageFile = $request->file('image');
+        $imageService = new ImageService();
+        $imageService->update('/', $imageFile);
 
         $image->path = $request->input('name');
         $image->pet_id = $request->input('pet_id');
@@ -50,10 +61,14 @@ class ImageController extends Controller
      *
      * @param int $id
      * @return int
+     * @throws \League\Flysystem\FileNotFoundException
      */
     public function destroy(int $id)
     {
         $image = Image::find($id);
+
+        $imageService = new ImageService();
+        $imageService->delete($image->path);
 
         $image->delete();
 
