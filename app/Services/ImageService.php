407572f661aa85b243c3d6eb460a0a84d4594fd3
Turner Walters <turner@bythepixel.com>
2018-09-28 14:10:41 -0600

    Add ImageService

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -3,6 +3,9 @@
 namespace App\Services;
 
 use Imgix\UrlBuilder;
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
 
 
 /**
@@ -10,11 +13,71 @@ use Imgix\UrlBuilder;
  */
 class ImageService
 {
-    public function buildUrl(string $domain, int $width, int $height, string $fileName) {
-        $builder = new UrlBuilder($domain);
+    /**
+     * @param string $fileName
+     * @param int $width
+     * @param int $height
+     * @return string
+     */
+    public function buildUrl(string $fileName, int $width, int $height)
+    {
+        $builder = new UrlBuilder(getenv('IMGIX_DOMAIN'));
         $builder->setUseHttps(true);
         $params = array("w" => $width, "h" => $height);
         return $builder->createURL($fileName, $params);
     }
 
+    /**
+     * @param string $path
+     * @param string $contents
+     * @return bool
+     * @throws \League\Flysystem\FileExistsException
+     */
+    public function upload(string $path = "", string $contents = "")
+    {
+        $filesystem = $this->setupFilesystem();
+        return $filesystem->write($path, $contents);
+    }
+
+    /**
+     * @param string $path
+     * @param string $contents
+     * @return bool
+     * @throws \League\Flysystem\FileNotFoundException
+     */
+    public function update(string $path = "", string $contents = "")
+    {
+        $filesystem = $this->setupFilesystem();
+        return $filesystem->update($path, $contents);
+    }
+
+    /**
+     * @param string $path
+     * @return bool
+     * @throws \League\Flysystem\FileNotFoundException
+     */
+    public function delete(string $path)
+    {
+        $filesystem = $this->setupFilesystem();
+        return $filesystem->delete($path);
+    }
+
+    /**
+     * @return Filesystem
+     */
+    private function setupFilesystem()
+    {
+        $client = S3Client::factory([
+            'credentials' => [
+                'key'    => getenv('AWS_ACCESS_KEY'),
+                'secret' => getenv('AWS_SECRET_ACCESS_KEY'),
+            ],
+            'region' => 'us-east-1',
+            'version' => 'latest',
+        ]);
+
+        $adapter = new AwsS3Adapter($client, 'rescue-app-images');
+
+        return new Filesystem($adapter);
+    }
 }
